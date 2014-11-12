e18c75dae7f5e47e092100693f48d3ec8311f9fa
David Jonsson <david@djk.se>
2014-11-12 22:15:24 +0100

    corrected noise function some functions named added HTML file

--- sampSzLab.js
+++ sampSzLab.js
@@ -1,6 +1,14 @@
-var sampSzLab = function() {
+/**
+ * Visualizing sample sizes dependency on even Fourier transforms with noise
+ */
+
+var SampSzLab = function() {
+
+  "use strict";
+
   var cryptoObj = window.crypto || window.msCrypto;
-  this.boxMuller = function(mean, stdDev, n) {
+
+  this.boxMuller = function(func, stdDev, n) {
     if (!n) {
       n = 1;
     } 
@@ -9,9 +17,26 @@ var sampSzLab = function() {
     var i = 0, j = 0, rnd;
     var normDist = Float64Array(n);
     while (i < n - 1)  {
-      normDist[j++] = mean + stdDev * (Math.sqrt(-2 * Math.log((arr[i++] + 4294967296 * arr[i++])/18446744073709551616)) * Math.cos(2 * Math.PI * (arr[i++] + 4294967296 * arr[i++])/18446744073709551616));
+      normDist[j++] = mean + stdDev * (Math.sqrt(-2 * Math.log((arr[i++]/4294967296 + arr[i++])/18446744073709551616)) * Math.cos(2 * Math.PI * (arr[i++]/4294967296 + arr[i++])/18446744073709551616));
     }
     return normDist;
   }
+
+  // make a basis vector, based on func
+  // func must have period 2pi
+  this.frequencyBasis = function(func, normed) {
+       if(typeof(normed)==='undefined') normed = true;
+
+  }
+
+  //make a basis matrix
+  this.makeBasisSpectrum = function(dimensions){
+
+  }
+
+  this.makeSpectrum = function() {
+
+  }
+
 }
 
