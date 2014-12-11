92b6d0b9024cab2b9398f67c516102db9ac17227
David Jonsson <david@djk.se>
2014-12-11 16:03:44 +0100

    Box Muller transformation corrected and replaced

--- sampSzLab.js
+++ sampSzLab.js
@@ -7,9 +7,15 @@
 
 var SampSzLab = function(defaults) {
 
-  "use strict";
+//  "use strict";
 
   var cryptoObj = window.crypto || window.msCrypto;
+
+  this.random = function() {
+    var arr = new Uint32Array(2);
+    cryptoObj.getRandomValues(arr);
+    return arr[0]/4294967296 + arr[1]/18446744073709551616;
+  }
 
   this.basisFunc = Math.cos;
   this.normedBasis = true;
@@ -18,27 +24,49 @@ var SampSzLab = function(defaults) {
   this.period = 2 * Math.PI;
   this.interval = 2 * Math.PI;
 
-  this.boxMuller = function(n, func, stdDev) {
-    if (typeof n === 'undefined') n = 1;
-    if (typeof func === 'undefined') func = function(){return 1};
+  this.rndBMt = function() {
+    // Based on  http://www.protonfish.com/jslib/boxmuller.shtml
+    // modified for more uniform random()
+      var x = 0, y = 0, rds, c;
+
+      // Get two random numbers from -1 to 1.
+      // If the radius is zero or greater than 1, throw them out and pick two new ones
+      // Rejection sampling throws away about 20% of the pairs.
+      do {
+        x = this.random()*2-1;
+        y = this.random()*2-1;
+        rds = x*x + y*y;
+      }
+      while (rds == 0 || rds > 1)
+
+      // This magic is the Box-Muller Transform
+      c = Math.sqrt(-2*Math.log(rds)/rds);
+
+      // It always creates a pair of numbers. I'll return them in an array.
+      // This function is quite efficient so don't be afraid to throw one away if you don't need both.
+      return [x*c, y*c];
+  }
+
+  this.boxMuller = function(retArr, func, stdDev) {
+    if (typeof n === 'undefined') n = retArr.length;
+    if (typeof func === 'undefined') func = function(x){return 0};
     if (typeof stdDev === 'undefined') stdDev = 1;
 
-    var arr = new Uint32Array(3*n + 3);
+    var arr = new Uint32Array(3 * retArr.length + 3);
     cryptoObj.getRandomValues(arr);
     var i = 0, j = 0, rndR;
-    var normDist = new Float64Array(n+1);
-    while (j < n - 1)  {
+    var normDist = new Array(n+1);
+    while (j < n )  {
 //      normDist[j++] = func(j-1) + stdDev * (Math.sqrt(-2 * Math.log((arr[i++]/4294967296 + arr[i++])/18446744073709551616)) * Math.cos(2 * Math.PI * (arr[i++]/4294967296 + arr[i++])/18446744073709551616));
-      rndR = -2 * Math.log((arr[i++]/4294967296 + arr[i++])/18446744073709551616))
-      normDist[j++] =  func(j-1) + stdDev * (Math.sqrt(rndR * Math.sin(2 * Math.PI * (arr[i++]/4294967296 + arr[i++])/18446744073709551616));
-      normDist[j++] =  func(j-1) + stdDev * (Math.sqrt(rndR * Math.cos(2 * Math.PI * (arr[i++]/4294967296 + arr[i++])/18446744073709551616));
+      rndR = -2 * Math.log(arr[i++]/4294967296 + arr[i++]/18446744073709551616);
+      normDist[j++] =  func(j) + stdDev * (Math.sqrt(rndR * Math.sin(2 * Math.PI * (arr[i++]/4294967296 + arr[i++]/18446744073709551616))));
+      normDist[j++] =  func(j) + stdDev * (Math.sqrt(rndR * Math.cos(2 * Math.PI * (arr[i++]/4294967296 + arr[i++]/18446744073709551616))));
     }
     normDist.length = n;
     return normDist;
   }
 
   // make a basis vector, based on func
-  // func must have period 2pi
   this.frequencyBasis = function() {
 //    var freqVec = new Float64Array(this.sampSz);
     var freqVec = new Array(this.sampSz);
